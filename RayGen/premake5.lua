6097f23b081b8677e0a12a30e596b25d2eab8e8a
Ljamo <masterluka21@gmail.com>
2023-01-28 18:38:40 +1100

    Setup basic gui and rendering.

--- RayGen/premake5.lua
+++ RayGen/premake5.lua
@@ -1,4 +1,4 @@
-project "WalnutApp"
+project "RayGen"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "RayGen"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "RayGen"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "RayGen"
