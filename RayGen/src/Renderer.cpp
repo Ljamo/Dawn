7ad4e626e0cbc17a43c588eac3aa4570e028d3ab
Ljamo <masterluka21@gmail.com>
2023-02-03 20:39:36 +1100

    Added lighting and shading.

--- RayGen/src/Renderer.cpp
+++ RayGen/src/Renderer.cpp
@@ -2,6 +2,21 @@
 
 #include "Walnut/Random.h"
 
+namespace Utils {
+
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (color.r * 255.0f);
+		uint8_t g = (color.g * 255.0f);
+		uint8_t b = (color.b * 255.0f);
+		uint8_t a = (color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -29,7 +44,10 @@ void Renderer::Render()
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f; // - -> +
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
@@ -37,12 +55,12 @@ void Renderer::Render()
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
 
-	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 	// rayDirection = glm::normalize(rayDirection);
@@ -61,10 +79,24 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	// Quadratic formula discriminant:
 	// b^2 - 4ac
 
+
 	float discriminant = b * b - 4.0 * a * c;
 
-	if (discriminant >= 0.0f)
-		return 0xffff00ff;
+	if (discriminant < 0.0f)
+		return glm::vec4(0, 0, 0, 1);
 
-	return 0xff000000;
+	// (-b +- sqrt(discriminant)) / 2a
+	float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+
+	glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+	glm::vec3 normal = glm::normalize(hitPoint);
+
+	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+
+	float d = glm::max(glm::dot(normal , -lightDir), 0.0f); // cos(angle)
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= d;
+	return glm::vec4(sphereColor, 1.0f);
 }
